f394474692a39199af2d8112aba6e59daf27f78c
miru <mirunoyume@naver.com>
2020-09-12 22:58:28 +0900

    data hw by miru, almost done except for no.4

---
+++ KIYAK_1.py
@@ -0,0 +1,17 @@
+def form(a,b,c,d):
+    num=a*d+b*c
+    den=b*d
+    mod = num%den
+    while mod > 0:
+        num = den
+        den = mod
+        mod = num%den
+    print(int(num/den),"/",int(b*d/den))
+
+    
+a,b=map(int, input("문자열 1 : ").split(","))
+c,d=map(int, input("문자열 2 : ").split(","))
+
+
+form(a,b,c,d)        
+
